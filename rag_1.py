d46cb3b2c7dd5e22553b319fbad37317ec7ef923
vishal pathak <vishalpathak842@gmail.com>
2025-04-19 09:38:13 +0530

    Refactor vector store initialization and retrieval process for improved clarity

--- rag_1.py
+++ rag_1.py
@@ -20,16 +20,40 @@ embedder = OpenAIEmbeddings(
     model="text-embedding-3-large",
     api_key= "OPENAI_API_KEY",
 )
+# this is for the injection part
 
-vector_store = QdrantVectorStore.from_documents(
-    documents=[],
+# vector_store = QdrantVectorStore.from_documents(
+#     documents=[],
+#     url="http://localhost:6333",
+#     collection_name="learning_langchain",
+#     embedding=embedder,
+# )
+
+# print("DOCS", len(docs))
+# print("SPLIT",len(split_doc))
+
+# vector_store.add_documents(documents=split_doc)
+print("Injection Done")
+
+retriver = QdrantVectorStore.from_existing_collection(
     url="http://localhost:6333",
     collection_name="learning_langchain",
     embedding=embedder,
 )
 
-# print("DOCS", len(docs))
-# print("SPLIT",len(split_doc))
+# search_result = retriver.similarity_search(
+#     query="What is FS Module?"
+# )
 
-vector_store.add_documents(documents=split_doc)
-print("Injection Done")
+# Print("Relevant Chunks", search_result)
+
+relevant_chunks = retriver.similarity_search(
+    query="What is FS Module?"
+)
+
+SYSTEM_PROMPT = """
+You are an helpful AI Assistant who responds base of the available context.
+
+Context:
+{relevant_chunks}
+"""
