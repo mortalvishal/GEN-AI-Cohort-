59d5335cb115bb04a7e898de99556e447783d5a9
vishal pathak <vishalpathak842@gmail.com>
2025-04-19 08:36:19 +0530

    Enhance PDF processing by adding text splitting functionality

--- rag_1.py
+++ rag_1.py
@@ -2,9 +2,18 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+
 pdf_path = Path(__file__).parent / "notes.pdf"
 
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()
 
-print(docs[10])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+)
+
+split_doc = text_splitter.split_documents(documents=docs)
+
+print("DOCS", len(docs))
+print("SPLIT",len(split_doc))
