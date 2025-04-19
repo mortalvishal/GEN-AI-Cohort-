c9da6aba5747252ae57d3ae08aa0d04a82811d04
vishal pathak <vishalpathak842@gmail.com>
2025-04-19 09:26:35 +0530

    Add Qdrant integration for document storage and injection

--- rag_1.py
+++ rag_1.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore 
 
 pdf_path = Path(__file__).parent / "notes.pdf"
 
@@ -20,5 +21,14 @@ embedder = OpenAIEmbeddings(
     api_key= "OPENAI_API_KEY",
 )
 
-print("DOCS", len(docs))
-print("SPLIT",len(split_doc))
+vector_store = QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="learning_langchain",
+    embedding=embedder,
+)
+
+# print("DOCS", len(docs))
+# print("SPLIT",len(split_doc))
+
+vector_store.add_documents(documents=split_doc)
+print("Injection Done")
