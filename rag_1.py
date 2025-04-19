570ec167e34a15618da85477f166f6f498c9f4d6
vishal pathak <vishalpathak842@gmail.com>
2025-04-19 08:39:30 +0530

    Add OpenAIEmbeddings initialization for document embedding

--- rag_1.py
+++ rag_1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_openai import OpenAIEmbeddings
 
 pdf_path = Path(__file__).parent / "notes.pdf"
 
@@ -14,6 +14,11 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 split_doc = text_splitter.split_documents(documents=docs)
+
+embedder = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+    api_key= "OPENAI_API_KEY",
+)
 
 print("DOCS", len(docs))
 print("SPLIT",len(split_doc))
